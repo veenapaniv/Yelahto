c18668ab64c9bf47632861c441226b25f086cef1
Veenapani V <veenapaniv@Veenapanis-MacBook-Air.local>
2019-03-18 02:00:04 -0500

    See Details added and modified the json from which data is fetched in landing page

--- js/landingPage.js
+++ js/landingPage.js
@@ -28,19 +28,36 @@ $(document).ready(function(){
     //on load function to bind Hamburger 
 	loadDataDetails();
     
+    //loadSearchDetails();
+    
 });
+
+function loadSearchDetails(){
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost/Yelahto/data/searchOrder.json',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success:function(jsonData){
+          console.log(jsonData.listOfOrders[0].adultSwamiPAX);  
+        },
+        error:function(jsonData){
+            console.log(jsonData.listOfOrders.adultSwamiPAX);
+        }
+    })
+}
 
 function loadDataDetails()
 {
      $.ajax({
     type: 'GET',
-    url: 'http://localhost/Yelahto/data/landing_page.json',
+    url: 'http://localhost/Yelahto/data/searchOrder.json',
+        //url: 'http://localhost/Yelahto/data/landing_page.json',
     contentType: 'application/json; charset=utf-8',
     dataType: 'json',
     success: function(jsonData) {
-      var pages=(jsonData.operatorDetails.length)/4;
-          //console.log(pages); 
-        //no.of pages
+      var pages=(jsonData.listOfOrders.length)/4;
+        
         if (pages == Math.ceil(pages)) 
             pages=pages;
           else 
@@ -54,16 +71,15 @@ function loadDataDetails()
                 next: 'Next',
                 prev: 'Prev',
                 onPageClick: function (event, page) {
-                    //fetch content and render here
-                    //$('#page-content').text('Page ' + page) + ' content here';
-                    //remove children before appending with new data
                             $(".history").children().remove();
                 for(var i=(4*(page-1));i<(4*(page-1))+4;i++)       
                      {                           
-                    if (i<jsonData.operatorDetails.length)
+                    if (i<jsonData.listOfOrders.length)
                         {
-                        
-                       // $(".history").addClass("row col-sm-6 parent"+i).appendTo("#page-content");
+                            var nonVegPax = jsonData.listOfOrders[i].adultNonVegPAX + jsonData.listOfOrders[i].childNonVegPAX;
+                            var jainPax = jsonData.listOfOrders[i].adultJainPAX + jsonData.listOfOrders[i].childJainPAX;
+                            var vegPax = jsonData.listOfOrders[i].adultVegPAX + jsonData.listOfOrders[i].childVegPAX
+                            var swamiPax = jsonData.listOfOrders[i].adultSwamiPAX + jsonData.listOfOrders[i].childSwamiPAX
                         var class1 = document.createElement("p");
                         $(class1).addClass("row col-sm-6 parent"+i).appendTo(".history");
 
@@ -78,8 +94,12 @@ function loadDataDetails()
                         $(class4).addClass("panel-heading").text("Date and Time").appendTo(".panel"+i);
 
                         var class5 = document.createElement("div");
-                        $(class5).addClass("panel-body").text(jsonData.operatorDetails[i].name).appendTo(".panel"+i)
-                        $(class5).addClass("panel-body").text(jsonData.operatorDetails[i].data).appendTo(".panel"+i)
+                        $(class5).addClass("panel-body").appendTo(".panel"+i);                        
+                        $(class5).append("<p>"+jsonData.listOfOrders[i].tourOperator+"</p>");
+                        $(class5).append("<p>"+"Pax:"+nonVegPax+jainPax+vegPax+swamiPax+"</p>");
+                        $(class5).append("<p>"+jsonData.listOfOrders[i].deliveryAddress+"</p>");
+                        $(class5).append("<p>"+"Non-Veg:"+nonVegPax+", Veg:"+vegPax+", Jain:"+jainPax+", Swami"+swamiPax+"</p>");    
+                        $(class5).append("<button type=\"button\" class=\"btn btn-primary btn-sm\" data-toggle=\"modal\" data-target=\"#myModal\">See Details</button> ");                  
                         }
                     }
                     
