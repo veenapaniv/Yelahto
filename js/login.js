98898333eb2f103d73ef55aa40d05a9e926cb727
Veenapani V <veenapaniv@Veenapanis-MacBook-Air.local>
2019-03-29 14:42:21 -0500

    modified login.js

--- js/login.js
+++ js/login.js
@@ -1,43 +1,33 @@
 $(document).ready(function () {
     $("#login-submit").click(function(e){ 
+	var authRequest = {
+	userName:"veenapani.v@gmail.com",
+	password: "veena123"
+};
          $.ajax({
-            type: 'GET',
-            url: 'http://localhost/Yelahto/data/login.json',
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            success: function(jsonData) {
-                var status = jsonData.status;
-            
-                if( status.toLowerCase() == 'OK'.toLowerCase()){
-                    $("#error-message").hide();
-                    window.location.assign("http://localhost/Yelahto/templates/LandingPage.html");
-                }  
-                else if( status.toLowerCase() != 'OK'.toLowerCase()){
-                    $("#error-message").show();                    
-                }    
+            type: "POST",
+			
+            url: 'http://testcore.apnnaghar.co.in/auth/login',
+			 dataType: "json",
+			data:JSON.stringify(authRequest),
+             contentType: 'application/json; charset=utf-8',
+			
+                    success: function (jsonData) {
+                        console.log(jsonData);
+                        var status = jsonData.status;
 
-            },
-            error: function(data) {
-                console.log('Error loading the image');
-            }
-        });
-    });
-    
-//    $("#login-submit").click(function(e){
-//            $.ajax({
-//                type:'Get',
-//                url: 'http://testcore.apnnaghar.co.in/auth/login',
-//                dataType: 'json',
-//                success: function(jsonData) {
-//                    console.log(jsonData.status);
-//                },
-//                error:function(jsonData){
-//                    console.log(jsonData.status);
-//                }
-//                
-//            });
-//                             
-//    });
-//    
-    
+                        if( status.toLowerCase() == 'OK'.toLowerCase()){
+                            $("#error-message").hide();
+                            window.location.assign("http://localhost/Yelahto/templates/LandingPage.html");
+                        }  
+                        else if( status.toLowerCase() != 'OK'.toLowerCase()){
+                            $("#error-message").show();                    
+                        }    
+                        
+                    }
+                    error: function(data) {
+                        console.log('Error loading the image');
+                    }
+           
+    }); });
 });
